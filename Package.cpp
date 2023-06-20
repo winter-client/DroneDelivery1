bdf915a780d7c20efa5f923d7114a5cd6d050da1
YZH2201554 <135527387+YZH2201554@users.noreply.github.com>
2023-06-20 15:54:47 +0800

    Update Package.cpp

--- Package.cpp
+++ Package.cpp
@@ -1,1 +1,28 @@
 #include "Package.h"
+
+Package::Package(const std::string& name, const std::string& dropOffPoint, const std::string& dropOffDeadline)
+    : name(name), dropOffPoint(dropOffPoint), dropOffDeadline(dropOffDeadline), assignedToDrone(false) {}
+
+const std::string& Package::getName() const {
+    return name;
+}
+
+const std::string& Package::getDropOffPoint() const {
+    return dropOffPoint;
+}
+
+const std::string& Package::getDropOffDeadline() const {
+    return dropOffDeadline;
+}
+
+bool Package::isAssignedToDrone() const {
+    return assignedToDrone;
+}
+
+void Package::assignToDrone() {
+    assignedToDrone = true;
+}
+
+void Package::unassignFromDrone() {
+    assignedToDrone = false;
+}
