67dc77e82956bdf01b3176e113d16d36f4795a2e
YZH2201554 <135527387+YZH2201554@users.noreply.github.com>
2023-06-20 15:54:03 +0800

    Update Drone.cpp

--- Drone.cpp
+++ Drone.cpp
@@ -1,1 +1,28 @@
 #include "Drone.h"
+
+Drone::Drone(const std::string& name, const std::string& destination, const std::string& rechargeDeadline)
+    : name(name), destination(destination), rechargeDeadline(rechargeDeadline), hasPackage(false) {}
+
+const std::string& Drone::getName() const {
+    return name;
+}
+
+const std::string& Drone::getDestination() const {
+    return destination;
+}
+
+const std::string& Drone::getRechargeDeadline() const {
+    return rechargeDeadline;
+}
+
+bool Drone::hasPackageAssigned() const {
+    return hasPackage;
+}
+
+void Drone::assignPackage() {
+    hasPackage = true;
+}
+
+void Drone::unassignPackage() {
+    hasPackage = false;
+}
